adf74a44b6a4bd02e8495ef1b2ad331ddd8c0e50
Thomas <hirtzt@gmail.com>
2021-06-13 19:14:00 +0200

    Fix lint

--- variational_autoencoder/__main__.py
+++ variational_autoencoder/__main__.py
@@ -2,30 +2,35 @@ import argparse
 from variational_autoencoder.dashboard import get_app_dashboard
 
 
-default_seed = 4
-default_num_epochs = 20
-default_file_name = None  # 'VAE_20210524_213001.pt'
+DEFAULT_SEED = 4
+DEFAULT_NUM_EPOCHS = 20
+DEFAULT_FILE_NAME = None  # 'VAE_20210524_213001.pt'
+
 
 parser = argparse.ArgumentParser(
     description='PyTorch Variational AutoEncoder',
     usage='Use "python variational-autoencoder --help" for more information'
 )
 parser.add_argument(
-    '--file_name', default=default_file_name, type=str, metavar='',
+    '--file_name', default=DEFAULT_FILE_NAME, type=str, metavar='',
     help='Name used for loading the model\'s weights (default: None)'
 )
 parser.add_argument(
-    '--num-epochs', default=default_num_epochs, type=int, metavar='',
+    '--num-epochs', default=DEFAULT_NUM_EPOCHS, type=int, metavar='',
     help='Number of training epochs (default: %(default)s)'
 )
 parser.add_argument(
-    '--seed', default=default_seed, type=int, metavar='',
+    '--seed', default=DEFAULT_SEED, type=int, metavar='',
     help='Seed used for pytorch (default: %(default)s)'
 )
 
 
 args = parser.parse_args()
-app = get_app_dashboard(num_epochs=args.num_epochs, file_name=args.file_name, seed=args.seed)
+app = get_app_dashboard(
+    num_epochs=args.num_epochs,
+    file_name=args.file_name,
+    seed=args.seed
+)
 
 
 if __name__ == "__main__":
